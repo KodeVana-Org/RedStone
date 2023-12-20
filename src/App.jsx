0569cd5ba329c431b7ee700d9840e2c2e1a9c9db
Sahisway <sahilswargiary.personal@gmail.com>
2023-12-21 03:13:27 +0530

    Add Profile and Cart Page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Home from './pages/Home.jsx';
 import Products from './pages/Products.jsx';
 import About from './pages/About.jsx';
 import Contact from './pages/Contact.jsx';
+import Cart from './pages/Cart.jsx';
+import Account from './pages/Account.jsx';
 
 function App() {
 
@@ -16,6 +18,8 @@ function App() {
           <Route path="/products" element={<Products />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/account" element={<Account />} />
         </Routes>
       </div>
     </Router>
