1375fe34cfa495d3b14ed57b2076a9f81d89e16e
usernamedinesh <borod9200@gmail.com>
2023-12-22 06:01:02 +0530

    fix userDetails,purchaseDetails

--- backend/src/controller/getProfile.js
+++ backend/src/controller/getProfile.js
@@ -6,39 +6,44 @@ const verifyToken = require('../middleware/verifyToken');
 
 
 exports.getProfile = async (req, res) => {
-    try {
-      const userId = req.user.id;
+  try {
+    const userId = req.user.id;
   
-      const userDetails = await User.findOne({ _id: userId });
-      const purchaseDetails = await ProductDetails.find({ user: userId }).populate('user').exec();
+    const userDetails = await User.find({ _id: userId });
+    console.log(userDetails);
   
-      let responseData = {};
+    const purchaseDetails = await ProductDetails.find({ user: userId }).populate('user').exec();
+    console.log(purchaseDetails);
   
-      if (purchaseDetails) {
-        // Send data from purchaseDetails if it exists
+    let responseData = {};
+  
+    if (purchaseDetails.length > 0) {
+      // Send data from purchaseDetails if it exists
+      responseData = {
+        message: 'Data received successfully',
+        purchaseDetails: purchaseDetails,
+      };
+    } else {
+      if (userDetails.length > 0) {
         responseData = {
           message: 'Data received successfully',
-          userDetails: null,
-          purchaseDetails: purchaseDetails || null,
+          userDetails: userDetails,
         };
       } else {
-        // If purchaseDetails is not present, send data from userDetails
         responseData = {
-          message: 'Data received successfully',
-          userDetails: userDetails || null,
-          purchaseDetails: null,
+          message: 'No data found',
         };
       }
-  
-      // Send the combined data
-      return res.status(200).json({
-        success: true,
-        message: "Data received",
-        responseData
-      });
-    } catch (error) {
-      console.error('Error fetching user profile:', error);
-      res.status(500).json({ error: 'Server error' });
     }
-  };
   
+    // Send the combined data
+    return res.status(200).json({
+      success: true,
+      message: 'Data received',
+      responseData,
+    });
+  } catch (error) {
+    console.error('Error fetching user profile:', error);
+    res.status(500).json({ error: 'Server error' });
+  }
+}
--- client/src/components/user/profile.jsx
+++ client/src/components/user/profile.jsx
@@ -5,12 +5,11 @@ import axios from "axios"
 
 import ProductsData from '../../assets/product'
 import UserDp from '../../assets/icon-user.png'
-import productsData from "../../assets/product"
 
 function Profile() {
   const [profileData, setProfileData] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [id, setid] = useState();
+  const [id, setid] = useState(null);
 
   const navigate = useNavigate();
   useEffect(() => {
@@ -32,19 +31,19 @@ function Profile() {
           },
         };
         const response = await axios.get('http://localhost:6969/api/get-data', config);
-        const data = response.data.responseData.userDetails;
+        const data = await response.data.responseData.userDetails;
         const data2 = response.data.responseData.purchaseDetails;
 
-        if (data) {
-          setProfileData(data);
-          console.log("Data is available:", data);
+        //got array of object
+        if (Array.isArray(data) && data.length > 0) {
+          setProfileData(data[0]);
+
         } else if (Array.isArray(data2) && data2.length > 0) {
           // Accessing the first element of purchaseDetails array
           setProfileData(data2[0].user);
           const allIds = data2.map(item => item.id);
-          console.log("All IDs:", allIds);
           setid(allIds);
-          // console.log("Data2 is available:", data2[0]);
+
         } else {
           console.log("No data available");
         }
@@ -59,10 +58,18 @@ function Profile() {
   }, []);
 
   useEffect(() => {
-    console.log("Updated Profile Data:", profileData);
+
   }, [profileData]);
 
-console.log("id is idddd", id)
+
+  const filterProductsByIds = () => {
+    if (!id) return []; // Return an empty array if 'id' is null or undefined
+    const filteredProducts = ProductsData.filter(product => id.includes(product.id));
+    return filteredProducts;
+  };
+  // Call the function to get the filtered products based on the IDs
+  const filteredProducts = filterProductsByIds();
+
   return (
     <div className="mx-96 my-16">
       {loading ? (
@@ -100,25 +107,25 @@ console.log("id is idddd", id)
       )}
 
 
-      <div>
-        <h3 className="mb-7 text-3xl font-medium">Your Orders</h3>
-        <div className="flex gap-7 justify-center">
-          {ProductsData.slice(4, 8).map((product) => (
-            <Link key={product.id} className="transition-all duration-300 hover:-translate-y-2" to={`/product/${product.id}`}>
-              <Img className="mb-1 h-80" src={product.image} alt={product.name} />
-              <h3 className="mb-1 text-gray-600">{product.name}</h3>
-              <div className="mb-1 flex gap-2 w-fit text-[#ff523b]">
-                <i className="fa fa-star" ></i>
-                <i className="fa fa-star" ></i>
-                <i className="fa fa-star" ></i>
-                <i className="fa fa-star-half-o" ></i>
-                <i className="fa fa-star-o" ></i>
-              </div>
-              <p className="text-gray-600">${product.price}</p>
-            </Link>
-          ))}
+     <div>
+  <h3 className="mb-7 text-3xl font-medium">Your Orders</h3>
+  <div className="flex gap-7 justify-center">
+    {filteredProducts.map((product) => (
+      <Link key={product.id} className="transition-all duration-300 hover:-translate-y-2" to={`/product/${product.id}`}>
+        <Img className="mb-1 h-80" src={product.image} alt={product.name} />
+        <h3 className="mb-1 text-gray-600">{product.name}</h3>
+        <div className="mb-1 flex gap-2 w-fit text-[#ff523b]">
+          <i className="fa fa-star"></i>
+          <i className="fa fa-star"></i>
+          <i className="fa fa-star"></i>
+          <i className="fa fa-star-half-o"></i>
+          <i className="fa fa-star-o"></i>
         </div>
-      </div>
+        <p className="text-gray-600">${product.price}</p>
+      </Link>
+    ))}
+  </div>
+</div>;
     </div>
   )
 }
